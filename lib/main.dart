a8554772f42e98f36673f504679bf4270a1b327c
Tom Vandenbroele <vandenbroeletom@gmail.com>
2022-05-13 20:09:27 +0200

    show qr in alertdialog

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:qr/qr.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'EPC QR',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('EPC QR'),
+          ),
+          body: Center(
+            child: EPCForm(),
+          ),
+        ));
+  }
+}
+
+class EPCForm extends StatelessWidget {
   final _formKey = GlobalKey<FormBuilderState>();
 
   List<Map<String, dynamic>> fields() {
@@ -67,49 +88,48 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'EPC QR',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('EPC QR'),
-          ),
-          body: Center(
-            child: FormBuilder(
-                key: _formKey,
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      ...fields().map((e) => Padding(
-                            padding: const EdgeInsets.all(8),
-                            child: FormBuilderTextField(
-                              name: e['name'],
-                              decoration:
-                                  InputDecoration(labelText: e['label']),
-                              initialValue: e['value'],
-                            ),
-                          )),
-                      ElevatedButton(
-                        child: Text('Create qr code'),
-                        onPressed: () {
-                          _formKey.currentState?.save();
+    return FormBuilder(
+        key: _formKey,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              ...fields().map((e) => Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: FormBuilderTextField(
+                      name: e['name'],
+                      decoration: InputDecoration(labelText: e['label']),
+                      initialValue: e['value'],
+                    ),
+                  )),
+              ElevatedButton(
+                child: Text('Create qr code'),
+                onPressed: () async {
+                  _formKey.currentState?.save();
 
-                          var values = _formKey.currentState!.value;
-                          var string = "";
+                  var values = _formKey.currentState!.value;
+                  var string = "";
 
-                          for (var field in fields()) {
-                            string += values[field['name']];
-                            string += "\n";
-                          }
+                  for (var field in fields()) {
+                    string += values[field['name']];
+                    string += "\n";
+                  }
 
-                          print(string);
-                        },
-                      )
-                    ],
-                  ),
-                )),
+                  final qrImage =
+                      QrImage(data: string, version: QrVersions.auto);
+
+                  final qrCode = QrCode(4, QrErrorCorrectLevel.H)
+                    ..addData(string);
+
+                  print(string);
+
+                  await showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(content: qrImage);
+                      });
+                },
+              )
+            ],
           ),
         ));
   }
